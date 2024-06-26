04cb3cb26f8bf5f9b4019d2f34736d082e7f893b
DonW16 <donw16@disroot.org>
2024-06-26 12:05:29 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # embed-fixer
 A Discord bot that will repost reddit, twitter to rxddit, vxtwitter, etc.
+
+## Create a Discord Bot:
+
+1. Go to the [Discord Developer Portal](https://discord.com/developers/)
+2. Create a new application.
+3. Create a bot user and get the bot token.
+4. Invite the bot to your server with the appropriate permissions (Read and Manage Messages).
+5. Enable Message Content Intent under the bot section.
+
+![alt text](image.png)
+
+## Run pod
+```bash
+podman run --network host -e DISCORD_BOT_TOKEN="yourtokenhere" embed-fixer
+```
---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+import re
+import os
+
+# Get the token from the environment variable
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+if TOKEN is None:
+    raise ValueError("No token found. Please set the DISCORD_BOT_TOKEN environment variable.")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+# Precompile the regex for better performance
+url_pattern = re.compile(r'(reddit\.com|instagram\.com|twitter\.com|youtube\.com|tiktok\.com|x\.com)')
+
+# Dictionary for replacements
+replacements = {
+    'reddit.com': 'rxddit.com',
+    'instagram.com': 'ddinstagram.com',
+    'twitter.com': 'vxtwitter.com',
+    'youtube.com': 'koutube.com',
+    'tiktok.com':  'vxtiktok.com',
+    'x.com':  'fixupx.com'
+}
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    original_content = message.content
+
+    # Perform the substitution in one pass
+    new_content = url_pattern.sub(lambda match: replacements[match.group(0)], original_content)
+
+    # Only perform I/O operations if changes were made
+    if new_content != original_content:
+        #await message.channel.send(f'{message.author.mention} posted: {new_content}')
+        await message.channel.send(f'{new_content}')
+        #await message.delete()
+
+client.run(TOKEN)
